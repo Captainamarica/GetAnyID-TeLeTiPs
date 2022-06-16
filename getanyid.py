d5a34a75822cf9f861a57389f3491f340ea327e9
NAVANJANA <104069319+Captainamarica@users.noreply.github.com>
2022-06-16 21:49:25 +0530

    Update getanyid.py

--- getanyid.py
+++ getanyid.py
@@ -1,27 +1,29 @@
-from pyrogram import Client, filters 
-from pyrogram.types import Chatpermission
-from pyrogram.types import InlinekeyboardButton, InlinekeyboardMarkup
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message
 import os
 
-bot=Client(
-    "Night Vission Bot",
+bot = Client(
+"Night Vission",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
     bot_token = os.environ["BOT_TOKEN"]
 )
 
-START_MESSAGE = "Im Night Vission Group Help Bot"
-START_MESSAGE_BUTTONS = ([
-[InlinekeyboardButton('SUPPORT', url='https://t.me/NightVisssion')
-])
+START_MESSAGE = "Heya im Test Bot"
+START_MESSAGE_BUTTONS = [
+[InlineKeyboardButton('CHANEL', url='https://t.me/NightVission')]
+]
+
 @bot.on_message(filters.command('start') & filters.private)
 def start(bot, message):
 	text = START_MESSAGE
-	reply_markup = InlinekeyboardMarkup(START_MESSAGE_BUTTONS)
-	message.reply(
+	reply_markup = InlineKeyboardMarkup(START_MESSAGE_BUTTONS)
+message.reply(
 	text=text,
-	reply_markup=reply_markup
+	reply_markup=reply_markup,
 	disable_web_page_preview=True
-
-print("Night Vission Bot Online Danger")
+)
+	
+print("he")
 bot.run()
